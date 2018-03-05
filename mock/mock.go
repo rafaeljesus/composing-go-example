852e26f01fcf830ba707e495fba7ccf75eba5a4b
Rafael Jesus <rafaelljesus86@gmail.com>
2018-03-05 09:33:42 +0100

    remove er prefix from mocks

--- mock/mock.go
+++ mock/mock.go
@@ -7,27 +7,27 @@ import (
 
 type (
 	HTTPClientMock struct {
-		HTTPPosterMock
-		HTTPGetterMock
+		HTTPPostMock
+		HTTPGetMock
 	}
 
-	HTTPPosterMock struct {
+	HTTPPostMock struct {
 		PostInvoked bool
 		PostFunc    func(url, contentType string, body io.Reader) (*http.Response, error)
 	}
 
-	HTTPGetterMock struct {
+	HTTPGetMock struct {
 		GetInvoked bool
 		GetFunc    func(url string) (*http.Response, error)
 	}
 )
 
-func (m *HTTPPosterMock) Post(url, contentType string, body io.Reader) (*http.Response, error) {
+func (m *HTTPPostMock) Post(url, contentType string, body io.Reader) (*http.Response, error) {
 	m.PostInvoked = true
 	return m.PostFunc(url, contentType, body)
 }
 
-func (m *HTTPGetterMock) Get(url string) (*http.Response, error) {
+func (m *HTTPGetMock) Get(url string) (*http.Response, error) {
 	m.GetInvoked = true
 	return m.GetFunc(url)
 }
